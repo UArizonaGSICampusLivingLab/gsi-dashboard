b8b621129cc611180bf523772f5aba5b1aba021b
Eric Scott <scottericr@gmail.com>
2023-12-04 16:11:54 -0700

    create dir if it doesn't exist

--- R/gsi_get_data.R
+++ R/gsi_get_data.R
@@ -1,15 +1,17 @@
 library(boxr)
 library(jose)
 library(dplyr)
+library(fs)
 
 
-gsi_get_data <- function(overwrite = TRUE) {
+gsi_get_data <- function(path = "data", overwrite = TRUE) {
   dir_id <- "233031886906"
   box_setwd(dir_id)
-
+  #create path if it doesn't exist
+  dir_create(path)
   box_ls() |>
     as_tibble() |> 
     filter(name == "gsi_living_lab_data.csv") |> 
     pull(id) |> 
-    box_dl(local_dir = "data", overwrite = overwrite)
+    box_dl(local_dir = path, overwrite = overwrite)
 }
