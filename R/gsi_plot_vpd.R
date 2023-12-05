567eefdeb376c06b756969a3cf2744f5b3c474e2
Eric Scott <scottericr@gmail.com>
2023-12-05 12:59:55 -0700

    function for plotting vp/vpd

---
+++ R/gsi_plot_vpd.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(tsibble)
+
+
+#' `data`: the dataset already filtered to a single site and a date range
+gsi_plot_vpd <- function(data) {
+  #use just atmospheric sensor data for this plot
+  data_atm <- 
+    data |> 
+    filter(str_starts(sensor, "ATM")) 
+  
+  ggplot(data_atm, aes(x = datetime)) +
+    #rather than map color to a column in the data or set it manually, we do a
+    #third thing—set it to a character string *inside* of aes().  This is a
+    #"trick" for creating a color legend
+    geom_line(aes(y = vapor_pressure.value, color = "VP"), alpha = 0.5, na.rm = TRUE) +
+    geom_line(aes(y = vpd.value, color = "VPD"), alpha = 0.5, na.rm = TRUE) +
+    #adjust colors manually
+    scale_color_manual(name = "", values = c("VP" = "blue", "VPD" = "darkgreen")) +
+    theme_bw() +
+    theme(axis.title.x = element_blank()) +
+    labs(y = "Vapor Pressure / VPD (kPa)")
+}
+
+
+#example:
+site_info <- read_csv("metadata.csv")
+data_full <- 
+  read_csv("data/gsi_living_lab_data.csv") |> 
+  right_join(site_info, by = join_by(device_sn, sensor, port))
+data_filtered <-
+  data_full |> 
+  filter(site == "Old Main", datetime > "2023-11-01", datetime < "2023-11-15")
+gsi_plot_vpd(data_filtered)
