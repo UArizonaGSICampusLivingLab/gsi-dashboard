a1543858eb0fc86f01a71a98c1c2424dc0482567
Eric Scott <scottericr@gmail.com>
2023-12-05 12:44:50 -0700

    update layout

--- app.R
+++ app.R
@@ -1,7 +1,5 @@
-
 # Setup -------------------------------------------------------------------
-# runs on app startup
-
+# runs on app start up
 library(shiny)
 library(bslib)
 library(bsicons)
@@ -11,23 +9,24 @@ library(tidyverse)
 library(boxr)
 library(glue)
 
-
+# Read in metadata
 site_info <- read_csv("metadata.csv")
 
-#download most recent data from Box
+# Download most recent data from Box
 box_auth_service()
 gsi_get_data()
-#joins with site info
+
+# Read in data and join with site info
 data_full <- 
   read_csv("data/gsi_living_lab_data.csv") |> 
   right_join(site_info)
-
 
 # UI ----------------------------------------------------------------------
 ui <- page_navbar(
   title = "GSI Living Lab",
   # fillable = FALSE, # make scrollable.  Try with and without this
   sidebar = sidebar(
+    # This could be a value_box instead of just plain text
     paste("Data last updated ", 
           format(max(data_full$datetime, na.rm = TRUE),
                  "%Y/%m/%d %H:%M")),
@@ -50,8 +49,27 @@ ui <- page_navbar(
     
   ),
   nav_panel(
-    "Timeseries",
-    
+    "Atmospheric",
+    card(
+      card_header("Air Temperature")
+    ),
+    card(
+      card_header("Precipitation")
+    ),
+    card(
+      card_header("Vapor Pressure and VPD")
+    )
+  ),
+  nav_panel(
+    "Soil"
+  ),
+  nav_panel(
+    "Environmental Plots" #plant-available water, other calculated values
+  ),
+
+  nav_panel(
+    "value box demo",
+
     layout_columns(
       height = "20%",
       uiOutput("stat_airtemp"),
@@ -74,12 +92,6 @@ ui <- page_navbar(
          card_body("plot goes here")
     )
   ),
-  nav_panel(
-    "Monthly Summary"
-  ),
-  nav_panel(
-    "Explore"
-  )
 )
 
 
@@ -93,32 +105,34 @@ server <- function(input, output, session) {
   })
   
   output$stat_airtemp <- renderUI({
-      airtemp <- data_filtered()$air_temperature.value
-      airtemp_vals <- 
-        c(
-          max(airtemp, na.rm = TRUE),
-          mean(airtemp, na.rm = TRUE),
-          min(airtemp, na.rm = TRUE)
-        ) |> 
-        round(2)
-      value_box(
-        title = "Air Temperature",
-        value =  HTML(glue("
+    airtemp <- data_filtered()$air_temperature.value
+    
+    airtemp_vals <- 
+      c(
+        max(airtemp, na.rm = TRUE),
+        mean(airtemp, na.rm = TRUE),
+        min(airtemp, na.rm = TRUE)
+      ) |> 
+      round(2)
+    
+    value_box(
+      title = "Air Temperature",
+      value =  HTML(glue("
            H: {airtemp_vals[1]} ºC<br>
            M: {airtemp_vals[2]} ºC<br>
            L: {airtemp_vals[3]} ºC
            ")),
-        showcase = bs_icon("thermometer")
-      )
-    })
+      showcase = bs_icon("thermometer")
+    )
+  })
   
   output$stat_soiltemp <- renderUI({
     soiltemp <- data_filtered()$soil_temperature.value
     soiltemp_vals <- 
       c(
-         max(soiltemp, na.rm = TRUE),
+        max(soiltemp, na.rm = TRUE),
         mean(soiltemp, na.rm = TRUE),
-         min(soiltemp, na.rm = TRUE)
+        min(soiltemp, na.rm = TRUE)
       ) |> 
       round(2)
     value_box(
@@ -133,11 +147,12 @@ server <- function(input, output, session) {
   })
   
   output$stat_precip<- renderUI({
-    precip <- 
+    
     precip_total <- 
       data_filtered()$precipitation.value |> 
       sum(na.rm = TRUE) |> 
       round(1)
+    
     value_box(
       title = "Total Precipitation",
       #TODO: check that units are correct
