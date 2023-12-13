9795f90fb2a91fa3660282f076097a2c2961351c
Eric Scott <scottericr@gmail.com>
2023-12-13 14:45:09 -0700

    use character input instead of fancy tidyeval

--- app/R/gsi_plot_soil.R
+++ app/R/gsi_plot_soil.R
@@ -2,16 +2,16 @@ library(Hmisc)
 library(dplyr)
 library(ggplot2)
 
-#' `data` is already filtered by date and site
-#' `yvar` is the column in data to plot on the y axis
-#' `depth` is either `0.5` or `0.15`
+#' `data`  data.frame; data already filtered by date and site
+#' `yvar`  character; the column name in data to plot on the y axis
+#' `depth` numeric; either `0.5` or `0.15`
 #' NOTE data doesn't exist for all combinations of site and depth and yvar
 gsi_plot_soil <- function(data, yvar, depth) {
   data |> 
     filter(depth_height_m == -depth) |> 
     mutate(month = month(datetime)) |> 
-    filter(!is.na({{yvar}})) |> 
-    ggplot(aes(x = month, y = {{yvar}}, linetype = basin, shape = basin, color = site)) +
+    filter(!is.na(.data[[yvar]])) |> 
+    ggplot(aes(x = month, y = .data[[yvar]], linetype = basin, shape = basin, color = site)) +
     stat_summary(fun.data = mean_sdl, position = position_dodge(width = 0.7)) +
     stat_summary(fun.y = mean, geom = "line", position = position_dodge(width = 0.7)) +
     scale_x_continuous(labels = \(x) month(x, label = TRUE)) +
@@ -23,5 +23,5 @@ gsi_plot_soil <- function(data, yvar, depth) {
 }
 
 ## to plug into shiny app, !!as.name("character_string")
-# gsi_plot_soil(data_full, yvar = !!as.name("soil_temperature.value"), depth = 0.15)
+gsi_plot_soil(data_full, yvar = "soil_temperature.value", depth = 0.15)
 
