513efd8f4fe4ce78ef31e46eb7e6e0cfff2aed54
Eric Scott <scottericr@gmail.com>
2023-12-14 13:26:46 -0700

    increase font size

--- app/R/0-theme_gsi.R
+++ app/R/0-theme_gsi.R
@@ -2,15 +2,17 @@ library(ggplot2)
 
 #define custom ggplot2 theme
 theme_gsi <- function() {
-  theme_linedraw() +
+  theme_linedraw(base_size = 14) + #change font size of all text
     theme(
       # panel.border = element_blank(),
       panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank()
+      panel.grid.minor = element_blank(),
     )
 }
 
 #set it as the default theme for all plots.
 theme_set(theme_gsi())
 
-gsi_site_colors <- c("Old Main" = "#AB0520", "Gould Simpson" = "#70B865", "Physics and Atmospheric Sciences" = "#1E5288")
+#defines colors for sites for use in scale_color_manual() etc.
+gsi_site_colors <- 
+  c("Old Main" = "#AB0520", "Gould Simpson" = "#70B865", "Physics and Atmospheric Sciences" = "#1E5288")
