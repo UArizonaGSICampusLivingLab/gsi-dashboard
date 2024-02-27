f0dfa2f6acb01d0b046aa3e0187770b1e9e9c543
Eric Scott <scottericr@gmail.com>
2024-02-27 11:28:07 -0700

    add range ribbons

--- app/R/gsi_plot_rh.R
+++ app/R/gsi_plot_rh.R
@@ -10,32 +10,35 @@ gsi_plot_rh <- function(data, daily = FALSE) {
     filter(str_starts(sensor, "ATM")) 
   
   if (isTRUE(daily)) {
-    #TODO: add ribbons for min/max?
     data_atm <-
       data_atm |> 
       mutate(datetime = floor_date(datetime, "day")) |> 
       dplyr::summarize(
-        relative_humidity.value = mean(relative_humidity.value),
+        rh_mean = mean(relative_humidity.value, na.rm = TRUE),
+        rh_min = min(relative_humidity.value, na.rm = TRUE),
+        rh_max = max(relative_humidity.value, na.rm = TRUE),
         .by = c(site, datetime)
       ) |> 
       #because mean(c(NA, NA, NA), na.rm = TRUE) results in Inf, we need to replace these with regular NAs for better plotting
-      mutate(relative_humidity.value = if_else(
-        !is.finite(relative_humidity.value),
-        NA,
-        relative_humidity.value
-      ))
+      mutate(across(starts_with("rh_"), \(x) if_else(!is.finite(x), NA, x)))
+    
+    p <- 
+      ggplot(data_atm, aes(x = datetime)) +
+      geom_line(aes(y = rh_mean, color = site), linewidth = 1) +
+      geom_ribbon(aes(ymin = rh_min, ymax = rh_max, fill = site), alpha = 0.2)
+  } else {
+    p <- 
+      ggplot(data_atm, aes(x = datetime, color = site)) +
+      geom_line(aes(y = relative_humidity.value), linewidth = 1, alpha = 0.75, na.rm = TRUE)
+    
   }
   
-  ggplot(data_atm, aes(x = datetime, color = site)) +
-    #rather than map color to a column in the data or set it manually, we do a
-    #third thing—set it to a character string *inside* of aes().  This is a
-    #"trick" for creating a color legend
-    geom_line(aes(y = relative_humidity.value), linewidth = 0.65, alpha = 0.5, na.rm = TRUE) +
+  p +
     #this makes the line go all the way to the edge of the plot.  I like this for timeseries
     scale_x_datetime(expand = c(0,0)) +
     scale_y_continuous(labels = scales::label_percent()) +
-    scale_color_manual(values = gsi_site_colors) + #defined in 0-theme_gsi.R
-    guides(color = "none") + #turn off legend
+    scale_color_manual(values = gsi_site_colors, aesthetics = c("fill", "color")) + #defined in 0-theme_gsi.R
+    guides(color = "none", fill = "none") + #turn off legend
     theme(axis.title.x = element_blank(), legend.position = "top") +
     labs(y = "Relative Humidity")
 }
