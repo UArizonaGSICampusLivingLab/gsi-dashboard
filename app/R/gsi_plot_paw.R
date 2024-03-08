1bf7525034cf6cd814b6a0d98e8a56389fe4c0d6
Eric Scott <scottericr@gmail.com>
2024-03-08 14:53:32 -0700

    summarize over depths and format as %

--- app/R/gsi_plot_paw.R
+++ app/R/gsi_plot_paw.R
@@ -5,10 +5,8 @@ gsi_plot_paw <- function(data) {
     #summarize sensors at same site, basin, and depth
     dplyr::summarize(
       paw.value = mean(paw.value),
-      .by = c(site, basin, depth_height_m, datetime)
+      .by = c(site, basin, datetime)
     ) |> 
-    #recode depth to be "0.5m" instead of -0.5 (for example)
-    mutate(depth = paste(-depth_height_m, "m")) |> 
     #recode basin to "basin" and "non-basin"
     mutate(basin = case_when(
       basin == "n" ~ "non-basin",
@@ -16,13 +14,13 @@ gsi_plot_paw <- function(data) {
       .default = NA
     ))
   
-  ggplot(plot_df, aes(x = datetime, y = paw.value, color = site)) +
+  ggplot(plot_df, aes(x = datetime, y = paw.value, color = site, linetype = basin)) +
     geom_line(alpha = 0.75, linewidth = 1, na.rm = TRUE) +
-    facet_grid(depth~basin) +
-    #TODO what are units for PAW?
-    labs(x = "", y = "Plant Avail. Water (UNITS??)") +
+    facet_wrap(~site) +
+    labs(x = "", y = "Plant Avail. Water", linetype = "") +
     scale_x_datetime(expand = c(0,0)) +
+    scale_y_continuous(labels = scales::label_percent(scale = 1)) +
     scale_color_manual(values = gsi_site_colors, aesthetics = c("fill", "color")) + #defined in 0-theme_gsi.R
-    guides(color = "none", fill = "none") + #turn off legend
+    guides(color = "none", fill = "none") +
     theme(axis.title.x = element_blank())
 }
